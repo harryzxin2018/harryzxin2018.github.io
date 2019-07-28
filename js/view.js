fe0d2a43b4f7ea70745ec78f5e7a0f1cb2d86a9b
Zixin Wang <harryzwang98@gmail.com>
2019-07-28 23:53:39 +0800

    update11:53

---
+++ js/view.js
@@ -0,0 +1,19 @@
+
+function check(){
+	document.getElementById("homework_today").innerHTML = localStorage.getItem("publish");
+
+}
+
+
+function login(){
+	var name = document.getElementById("name").value;
+	var password = document.getElementById("password").value;
+	if (name == "teacher_test" && password == "123456") {
+    window.open("teacher_page.html","_self");
+  } else {
+    alert("用户名或密码有误");
+  }
+
+
+
+}
